5133d7d12ae01754968d60802e6f1f210ec573c9
emonfromearth <qzjaman@gmail.com>
2022-01-23 13:11:20 +0600

    schema types

--- User.js
+++ User.js
@@ -1,8 +1,34 @@
 const mongoose = require('mongoose');
 
+const addressSchema = new mongoose.Schema({
+    street: String,
+    city: String,
+}
+);
+
 const userSchema = new mongoose.Schema({
     name: String,
-    age: Number
+    age: Number,
+    email: String,
+    createdAt: String,
+    updatedAt: String,
+    bestFriend: mongoose.SchemaTypes.ObjectId,
+    hobbies: [String],
+    address: addressSchema
 });
+
+// const userSchema = new mongoose.Schema({
+//     name: String,
+//     age: Number,
+//     email: String,
+//     createdAt: String,
+//     updatedAt: String,
+//     bestFriend: mongoose.SchemaTypes.ObjectId,
+//     hobbies: [String],
+//     address: {
+//         street: String,
+//         city: String,
+//     },
+// });
 
 module.exports = mongoose.model('User', userSchema);
--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
-const User = require('./User');
+const mongoose = require("mongoose");
+const { mainModule } = require("process");
+const User = require("./User");
 
 // mongoose.connect('mongodb://localhost/test', () => {
 //     console.log('connected to mongodb'), e => {
@@ -8,20 +9,32 @@ const User = require('./User');
 // });
 
 // If connection take some time or failed it will queue the queries so that we can drop the 2 callback functions.
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect("mongodb://localhost/test");
 
-run()
+run();
 async function run() {
-
-    const user = new User({
-        name: 'name',
-        age: 24
+  try {
+    const user = await User.create({
+      name: "Emon Sabbir",
+      age: 24,
+      email: "mail@omega.com",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      hobbies: ["hobby1", "hobby2"],
+      address: {
+        street: "street1",
+        city: "Dhaka",
+      },
     });
+    console.log("User saved!", user);
+  } catch (e) {
+    console.log(e.message);
+  }
 
-    try {
-        await user.save();
-        console.log('User saved!', user);
-    } catch (e) {
-        console.log(e.message);
-    }
+  // another method to create a user (document)
+  //   const user = new User({
+  //     name: "name",
+  //     age: 24,
+  //   });
+  //   await user.save();
 }
